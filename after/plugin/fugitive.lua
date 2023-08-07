5a6701e2bae0302facbcacc743ef700f93c47c81
Zsolt Fekete <zsoltf@me.com>
2023-08-06 18:26:40 -0700

    updates

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
 vim.keymap.set("n", "<leader>ga", ":Git commit --amend<CR>")
 vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
+vim.keymap.set("n", "<leader>gp", ":Git pull<CR>")
+vim.keymap.set("n", "<leader>gu", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gd", ":Gvdiff<CR>")
 vim.keymap.set("n", "<leader>gd1", ":Gvdiff HEAD~1<CR>")
 vim.keymap.set("n", "<leader>gd2", ":Gvdiff HEAD~2<CR>")
@@ -23,5 +25,6 @@ autocmd("BufWinEnter", {
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {buffer = bufnr, remap = false}
         vim.keymap.set("n", "q", ":close<CR>", opts)
+        vim.keymap.set("n", "cc", ":Git commit<CR>", opts)
     end,
 })
